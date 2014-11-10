e7261b63cc1e728ff27db07653beb5cf5fc72b4f
Sebastian Georgi <sgeorgi@sgeorgi.de>
2014-11-10 07:48:48 +0100

    FizzBuzz 02

--- src/main/scala/Kata.scala
+++ src/main/scala/Kata.scala
@@ -3,4 +3,15 @@
  */
 object Kata {
   def testString: String = "test String"
+
+  def apply(i: Int) = fizzBuzz(i)
+
+  def fizzBuzz(i: Int) = {
+    i match {
+      case i if i % 15 == 0 => "fizzbuzz"
+      case i if i % 5 == 0 => "buzz"
+      case i if i % 3 == 0 => "fizz"
+      case i => i
+    }
+  }
 }
--- src/test/scala/KataSpec.scala
+++ src/test/scala/KataSpec.scala
@@ -10,5 +10,33 @@ class KataSpec extends FunSpec {
         assert(Kata.testString == "test String")
       }
     }
+
+    describe("fizzBuzz") {
+      it("returns a number unless dividable by 3, 5 or 15") {
+        assert(Kata.fizzBuzz(1) == 1)
+        assert(Kata.fizzBuzz(2) == 2)
+        assert(Kata.fizzBuzz(4) == 4)
+        assert(Kata.fizzBuzz(7) == 7)
+        assert(Kata.fizzBuzz(8) == 8)
+      }
+
+      it("returns fizz when diviable by 3") {
+        assert(Kata.fizzBuzz(3) == "fizz")
+        assert(Kata.fizzBuzz(6) == "fizz")
+        assert(Kata.fizzBuzz(9) == "fizz")
+        assert(Kata.fizzBuzz(12) == "fizz")
+      }
+
+      it("returns buzz when diviable by 5") {
+        assert(Kata.fizzBuzz(5) == "buzz")
+        assert(Kata.fizzBuzz(10) == "buzz")
+        assert(Kata.fizzBuzz(20) == "buzz")
+      }
+
+      it("returns fizzbuzz when dividable by 15") {
+        assert(Kata.fizzBuzz(15) == "fizzbuzz")
+        assert(Kata.fizzBuzz(30) == "fizzbuzz")
+      }
+    }
   }
 }
